bcd31c00ae4c07f708e9619dcea8d83351681b16
href <thehref@192.168.1.15>
2025-02-03 00:43:10 +0300

    logo eklendi

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,13 +4,18 @@
  */
 
 // Assets 
+import logo from '@/assets/logo.png';
 
 const Logo = () => {
   return (
+    
+    <div className="flex items-center "><img src={logo} alt="TerraYazılım Logo " className="w-7 h-7 mr-2 lg:w-8 lg:h-8" />
     <a href="/" className="flex items-center     text-2xl md:text-3xl lg:text-3xl">
       
-      <span >TerraYazılım</span>
+      <span ><b>Terra</b>Yazılım</span>
     </a>
+    
+    </div>
   );
 }
 
