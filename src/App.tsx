c9245ed6dd9163238fc2c2a083cf66f7810e2b3a
href <thehref@192.168.1.15>
2025-02-03 00:23:21 +0300

    seni çok özledim eşşeğim

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-/**
- * @copyright 2025 @thehref
- * @license Apache-2.0
- */
+import { Routes, Route } from 'react-router-dom';
 
-
-// Routes 
-import { Routes,Route } from 'react-router-dom';
-
-// Components
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
-// Pages
-import İletisim from "@/pages/iletisim";
+import İletisim from '@/pages/iletisim';
 import Home from '@/pages/home';
 import Cozum from '@/pages/cozum';
 import Hakkimizda from '@/pages/hakkimizda';
@@ -20,28 +11,22 @@ import Referans from '@/pages/referans';
 import Ucret from '@/pages/ucretler';
 import P404 from '@/pages/p404';
 
-
-
-
 const App = () => {
   return (
-    <div className='relative isolate overflow-hidden'> 
-    <Header/>
-
-
-  <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/ucretler' element={<Ucret/>} />
-    <Route path='/iletisim' element={<İletisim/>} />
-    <Route path='/referans' element={<Referans/>} />
-    <Route path='/cozumlerimiz' element={<Cozum/>} />
-    <Route path='/hakkimizda' element={<Hakkimizda/>} />
-    <Route path='*' element={<P404/>} />
-
-  </Routes>
-  <Footer/>
+    <div className="relative isolate overflow-hidden">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ucretler" element={<Ucret />} />
+        <Route path="/iletisim" element={<İletisim />} />
+        <Route path="/referans" element={<Referans />} />
+        <Route path="/cozumlerimiz" element={<Cozum />} />
+        <Route path="/hakkimizda" element={<Hakkimizda />} />
+        <Route path="*" element={<P404 />} />
+      </Routes>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
